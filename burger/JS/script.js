4050b40ac79ecb6b994e8d4b4f9ed4dedb9033a6
hiroshi-git147 <116545212+hiroshi-git147@users.noreply.github.com>
2023-01-14 19:29:29 +0900

    update script.js

--- burger/JS/script.js
+++ burger/JS/script.js
@@ -5,26 +5,38 @@ var cat = document.querySelector('#cat');
 
 
 button.addEventListener('click', () => {
-	console.log('click');
-	var cheeseburger = document.createElement('div');
-	// cheeseburger.innerHTML = 'cheeseburger';
-	cheeseburger.classList.add('cheeseburger');
-	cat.appendChild(cheeseburger);
+//    デバッグ用
+    console.log('click');
+
+//    バーガーを生成する
+    var cheeseburger = document.createElement('div');
+    cheeseburger.classList.add('cheeseburger');
+    cat.appendChild(cheeseburger);
+
+//    バーガーが一つ以上ある場合バーガーを削除するボタン生成
+    var cheeseburgers = document.getElementsByClassName('cheeseburger');
+//  デバッグ用
+    console.log("バーガーが追加されました、数：" + cheeseburgers.length + "個");
+    if(cheeseburgers.length <= 1) {
+	remove_button.style.display = "inline-block";
+    }
+
 });
 
-function set_display() {
-	var cheeseburgers = document.getElementsByClassName('cheeseburger');
-	if(cheeseburgers.length <= 2) {
-		remove_button.style.display = "inline-block";
-	} else if(cheeseburgers.length == 1) {
-		remove_button.style.display = "none";
-	}
-}
 
 remove_button.addEventListener('click', () => {
-	console.log('clicked');
-	var cheeseburgers = document.getElementsByClassName('cheeseburger');
-	cheeseburgers[0].classList.remove('cheeseburger');
-	console.log(cheeseburgers.length);
-});
+//  デバッグ用
+    console.log('clicked');
 
+//  バーガーを削除する
+    var cheeseburgers = document.getElementsByClassName('cheeseburger');
+    cheeseburgers[0].classList.remove('cheeseburger');
+    console.log("バーガーが減りました、数：" + cheeseburgers.length + "個");
+
+//    バーガーがなくなったら削除ボタンをなくす
+    if(cheeseburgers.length == 0) {
+	//  デバッグ用
+	remove_button.style.display = "none";
+	console.log('バーガーがなくなりました');
+    }
+});
